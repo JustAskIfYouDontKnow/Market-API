8ff57c6f3c5aa043e205915a805e89f80da7e6d0
username <gold.like.simple@gmail.com>
2023-03-02 10:49:10 +1000

    [+] added models ( User, Order, Product ) to DbContext in PostgresContext class

---
+++ Market.API.Database/PostgresContext.cs
@@ -0,0 +1,25 @@
+﻿using Market.API.Database.Order;
+using Market.API.Database.Product;
+using Market.API.Database.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.API.Database
+{
+    public class PostgresContext : DbContext
+    {
+        public PostgresContext(DbContextOptions<PostgresContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<UserModel> User { get; set; }
+
+        public DbSet<OrderModel> Order { get; set; }
+        
+        public DbSet<ProductModel> Product { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
