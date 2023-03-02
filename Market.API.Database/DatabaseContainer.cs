2513f2c65d5952ceb8f84253c42fd84e072dc46c
User <gold.like.simple@gmail.com>
2023-03-02 19:32:42 +1000

    [+] ProductRepository, OrderService for DataBase Container

--- Market.API.Database/DatabaseContainer.cs
+++ Market.API.Database/DatabaseContainer.cs
@@ -1,5 +1,6 @@
-using Market.API.Database.Order;
+using Market.API.Database.OrderProduct;
 using Market.API.Database.Product;
+using Market.API.Database.Services;
 using Market.API.Database.User;
 
 namespace Market.API.Database;
@@ -7,14 +8,20 @@ namespace Market.API.Database;
 public class DatabaseContainer : IDatabaseContainer
 {
     public IUserRepository User { get; set; }
-    public IOrderRepository Order { get; set; }
     public IProductRepository Product { get; set; }
+    public IOrderProductModelRepo OrderProduct { get; set; }
+    
+    public OrderService OrderService { get; set; }
 
 
     public DatabaseContainer(PostgresContext db)
     {
         User = new UserRepository(db);
-        Order = new OrderRepository(db);
         Product = new ProductRepository(db);
+        OrderProduct = new OrderProductModelRepo(db);
+        OrderService = new OrderService(
+            new UserRepository(db),
+            new ProductRepository(db),
+            new OrderProductModelRepo(db));
     }
 }
--- Market.API.Database/IDatabaseContainer.cs
+++ Market.API.Database/IDatabaseContainer.cs
@@ -1,5 +1,6 @@
-using Market.API.Database.Order;
+using Market.API.Database.OrderProduct;
 using Market.API.Database.Product;
+using Market.API.Database.Services;
 using Market.API.Database.User;
 
 namespace Market.API.Database;
@@ -7,6 +8,8 @@ namespace Market.API.Database;
 public interface IDatabaseContainer
 {
     public IUserRepository User { get; set; }
-    public IOrderRepository Order { get; set; }
     public IProductRepository Product { get; set; }
+    public IOrderProductModelRepo OrderProduct { get; set; }
+    
+    public OrderService OrderService { get; set; }
 }
